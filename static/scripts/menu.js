5e060566da45bfdb1a35d73505906c36a1f21406
travs <alvincases18.ac@gmail.com>
2018-04-06 00:17:14 +0800

    Issue #32. Delete old javascript file for humburger display.

--- static/scripts/menu.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-    document.getElementById("side-navigation").style.width = "100%";
-}
-
-function closeNav() {
-    document.getElementById("side-navigation").style.width = "0";
-}
